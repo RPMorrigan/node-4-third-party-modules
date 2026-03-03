3e3e58104d1237830295fee8ac7e872c831463ab
RPMorrigan <morriganpriscilla@gmail.com>
2026-03-03 11:48:40 -0700

    checking in with phil

--- sign-finder/sign-finder.js
+++ sign-finder/sign-finder.js
@@ -5,3 +5,13 @@ Create a Node.js app that determines the astrological and zodiac signs for the u
 
 Read the instructions in the README.md file in this folder.
 ---------------------------- */
+
+import horoscope from 'horoscope';
+
+let userMonth = Number(process.argv[2]);
+let userDay = Number(process.argv[3]);
+// let userYear = Number(process.argv[4])
+
+console.log(userDay, userMonth);
+
+console.log(horoscope.getSign({ Month: userMonth, Day: userDay }));
